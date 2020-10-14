d44f6dbbd8dab16df50785b85998b3ff2ded8443
Colaresi <mpc61@AS-C02YK3YNJGH6.local>
2020-10-13 22:55:33 -0400

    added newMessages.py for new message addition

---
+++ newMessages.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+"""
+Add two vector messages together
+Input
+  None
+
+Output
+  print sum of two vector messages
+
+"""
+
+import vector
+
+MSG1 = [12.67, 5.121, 3.162]
+MSG2 = [2.722, -9.99, 2.611]
+
+print(vector.add(MSG1, MSG2))
