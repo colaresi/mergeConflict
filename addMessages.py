4d1d2a2b47a3b2a73d0f961ecdde3caf4428dd0f
Colaresi <mpc61@AS-C02YK3YNJGH6.local>
2020-10-13 21:02:47 -0400

    added file to add messages as vectors: addMessages.py

---
+++ addMessages.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+"""
+Add two vector messages together
+Input
+  None
+
+Output
+  print sum of two vector messages
+
+"""
+
+import vector
+
+MSG1 = [2.32, 2.110, 5.345]
+MSG2 = [-2.121, -2.99, -5.134]
+
+print(vector.add(MSG1, MSG2))
---
+++ vector.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+"""
+functions to work with vectors
+
+ - add
+ - subtract
+ - scale
+ - dot
+
+if run as __main__
+input
+  d -- the dimensionality of the space for the test
+output
+  None if tests passed, exception if tests not passed
+"""
+
+from typing import List
+
+# create Vector type for type hinting
+Vector = List[float]
+
+
+def add(v: Vector, w: Vector) -> Vector:
+    "add two vectors"
+    assert len(v) == len(w), "vectors must be the same length!"
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def subtract(v: Vector, w: Vector) -> Vector:
+    "subtract two vectors"
+    assert len(v) == len(w), "vectors must be the same length!"
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def scale(c: float, v: Vector) -> Vector:
+    "scale a vector v by a scalar c"
+    assert len(c) == 1, "need a scalar!"
+    return [c * v_i for v_i in v]
+
+
+def dot(v: Vector, w: Vector) -> float:
+    "compute dot product of 2 vectors"
+    assert len(v) == len(w), "vectors must be the same lenght!"
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+if __name__ == "__main__":
+    v1 = [1, 2, 3]
+    v2 = [4, 5, 6]
+    assert add(v1, v2) == [5, 7, 9], "something wrong with add"
+    assert subtract(v1, v2) == [3, 3, 3], "something wrong with subtract"
+    assert scale(2, v1) == [2, 4, 6], "something wrong with scale"
+    assert dot(v1, v1) == 14, "something wrong with dot"
